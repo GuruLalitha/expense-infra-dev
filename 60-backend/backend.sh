af56f5c9a4de3981c320f5b86eafe7b672b997e8
GuruLalitha <guru.lalitha90@gmail.com>
2025-04-28 18:51:35 +0530

    ansible roles

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
- 
- dnf install ansible -y
- #push
- #ansible-playbook -i inventory mysql.yaml
+
+dnf install ansible -y
+#push
+#ansible-playbook -i inventory mysql.yaml
 
 
- #pull
- ansible-pull -i localhost, -U https://github.com/GuruLalitha/expense-ansible-roles-tf.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
+#pull
+ansible-pull -i localhost, -U https://github.com/GuruLalitha/expense-ansible-roles-tf.git main.yaml -e COMPONENT=backend -e ENVIRONMENT=$1
