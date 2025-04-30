e34501c11ecfe614ed1317aa0b064a3a76844723
GuruLalitha <guru.lalitha90@gmail.com>
2025-04-30 10:44:22 +0530

    infra dev

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -58,6 +58,15 @@ module "app_alb_sg" {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags = var.common_tags
 }
+module "web_alb_sg" {
+    source = "git::https://github.com/GuruLalitha/terraform-aws-securitygroup.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "web_alb"
+    sg_description = "Created for frontend ALB in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
 
 # APP ALB accepting traffic from bastion
 resource "aws_security_group_rule" "app_alb_bastion" {
@@ -151,6 +160,24 @@ resource "aws_security_group_rule" "backend_vpn" {
   source_security_group_id = module.vpn_sg.sg_id
   security_group_id = module.backend_sg.sg_id
 }
+
+resource "aws_security_group_rule" "backend_vpn_http" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = module.vpn_sg.sg_id
+  security_group_id = module.backend_sg.sg_id
+}
+
+resource "aws_security_group_rule" "backend_app_alb" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = module.app_alb_sg.sg_id
+  security_group_id = module.backend_sg.sg_id
+}
 resource "aws_security_group_rule" "mysql_backend" {
   type              = "ingress"
   from_port         = 3306
@@ -158,4 +185,12 @@ resource "aws_security_group_rule" "mysql_backend" {
   protocol          = "tcp"
   source_security_group_id = module.backend_sg.sg_id
   security_group_id = module.mysql_sg.sg_id
+}
+resource "aws_security_group_rule" "web_alb_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks =  ["0.0.0.0/0"]
+  security_group_id = module.web_alb_sg.sg_id
 }
--- 80-web-alb/variables.tf
+++ 80-web-alb/variables.tf
@@ -14,8 +14,15 @@ variable "common_tags" {
         Environment = "dev"
         Terraform = "true"
     }
+  
 }
+
+variable "zone_id" {
+    default = "Z06864542GI2Z8TX8B5AP"
+  
+}
+
 variable "domain_name" {
     default = "gurudevops.online"
-
+  
 }
